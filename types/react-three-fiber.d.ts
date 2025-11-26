d71dbf072f072c19fdfbcbca8a47359d8e98df44
Artreeus <142108262+Artreeus@users.noreply.github.com>
2025-11-26 21:57:58 +0600

    feat : build error fix

---
+++ types/react-three-fiber.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="@react-three/fiber" />
+
+import { ThreeElements } from '@react-three/fiber';
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements extends ThreeElements {}
+  }
+}
+
